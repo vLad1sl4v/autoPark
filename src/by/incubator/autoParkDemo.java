c55eef80ffcc1ecab1179d233de9383a398b47cf
vLad1sl4v <uladzislau.kavalchuk@gmail.com>
2022-04-29 21:59:24 +0300

    lvl 7 fixes

--- src/by/incubator/autoParkDemo.java
+++ src/by/incubator/autoParkDemo.java
@@ -2,11 +2,16 @@ package by.incubator;
 
 import by.incubator.carWash.CarWashQueue;
 import by.incubator.vehicle.*;
-
+import by.incubator.carWash.CarWashQueue;
+import by.incubator.vehicle.*;
 import java.util.List;
+import java.util.Queue;
 
 public class autoParkDemo {
     private static final String path = "./src/by/incubator/data/";
+    private static final String TYPES_PATH = "src/by/incubator/data/";
+    private static final String VEHICLES_PATH = "src/by/incubator/data/";
+    private static final String RENTS_PATH = "src/by/incubator/data/";
 
     public static void main(String[] args) {
         VehicleCollection vehicleCollection = new VehicleCollection(path, path, path);
@@ -15,43 +20,30 @@ public class autoParkDemo {
         List<Vehicle> vehicles = vehicleCollection.loadVehicles("vehicles.csv");
         List<Rent> rents = vehicleCollection.loadRents("rents.csv");
 
-        vehicleCollection.display();
-
         CarWashQueue<Vehicle> queue = initCarWashQueue(vehicles);
         washAllCars(queue);
 
         System.out.println(queue.size() + " Machines left in queue");
+
     }
 
-    private static void washAllCars(CarWashQueue<Vehicle> queue) {
-        int size = queue.size();
-
-        for (int i = 0; i < size; i++) {
-            washCar(queue);
-        }
-    }
-
-    private static void washCar(CarWashQueue<Vehicle> queue) {
-        Vehicle washedCar = queue.dequeue();
-
-        System.out.println(washedCar.getModelName() + " washed");
-    }
-
-
-    private static CarWashQueue<Vehicle> initCarWashQueue(List<Vehicle> vehicles) {
+    static CarWashQueue<Vehicle> initCarWashQueue(List<Vehicle> vehicles) {
         Vehicle[] vehiclesArr = vehicles.toArray(new Vehicle[]{});
 
         return new CarWashQueue<>(vehiclesArr);
     }
 
-    static class Helper{
-        static <T> void printArray(T[] arr) {
-            for (int i = 0; i < arr.length; i++) {
-                    System.out.println(arr[i]);
-            }
+    static void washAllCars(CarWashQueue<Vehicle> queue) {
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            washCar(queue);
         }
     }
+
+    static void washCar(CarWashQueue<Vehicle> queue) {
+        Vehicle washedCar = queue.dequeue();
+        System.out.println(washedCar.getModelName() + " washed");
+    }
+
 }
-
-
 
